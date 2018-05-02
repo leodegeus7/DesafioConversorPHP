fd5c194829d99d7395fdc69e0c7c0c8d9b0dac80
Leonardo Geus <leonardodegeus@gmail.com>
2018-05-02 13:54:44 -0300

    Updated conversor.php

---
+++ conversor.php
@@ -0,0 +1,12 @@
+<?php
+function conversaoTemperatura($temp_celsius) {
+    $temp_fahrenheit = 0
+    
+   
+    return $temp_fahrenheit;
+}
+
+conversaoTemperatura(32);
+conversaoTemperatura(20);
+conversaoTemperatura(-5);
+?>
